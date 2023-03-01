21cca2fad49318a2eef450e75d711f000db62565
Pedro <contato.pedromoc@gmail.com>
2023-03-01 14:29:44 -0300

    Adicionados usuarios a lista do db.json

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 import axios from "./node_modules/axios/dist/esm/axios.js";
 
-const AXIOS_INSTANCE = axios.create({
+/*const AXIOS_INSTANCE = axios.create({
   baseUrl: "https://rickandmortyapi.com/api",
-  timeout: 1000
-});
+  timeout: 1000,
+  headers: {
+    Access-Control-Allow-Origin: https://normal-website.com
+  }
+});*/
 
 axios
-  .get("https://rickandmortyapi.com/api")
+  .get("https://my-json-server.typicode.com/pedrohsouza20/axios-aprendizado/", {
+    headers: {
+      "Access-Control-Allow-Origin": "https://my-json-server.typicode.com",
+    },
+  })
   .then((data) => console.log(data.data))
   .catch((err) => console.log(err));
 
